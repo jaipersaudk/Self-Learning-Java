116d9d95404c5a09aa9dfc0963cd8cd465ac8da5
kevinJ <jaipersaudk@gmail.com>
2019-03-22 11:54:42 -0400

    Hello World

---
+++ Basics/HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld { //defining a new class called HelloWorld. HelloWorld is the name of the class
+  public static void main(String[] args) { // every application must contain a main method by this signature
+    /*
+    * public: So that JVM can execute the method from anywhere.
+    * static: Main method is to be called without object.
+      The modifiers public and static can be written in either order.
+    * void: The main method doesn't return anything.
+    * main(): Name configured in the JVM.
+    * String[]: The main method accepts a single argument:
+      an array of elements of type String.
+    */
+    System.out.println("Hello World");
+    /*
+     * Outputs are accomplished by the built in function println()
+     * System is a predefined class that provides access to the System
+     * out is the variable of type output stream that is connected to the console
+    */
+  }
+}
--- first_steps.java
+++
@@ -1,5 +0,0 @@
-public class first_steps {
-  public static void main(String[] args) {
-    System.out.println("Hello World"); //how to print a message
-  }
-}
