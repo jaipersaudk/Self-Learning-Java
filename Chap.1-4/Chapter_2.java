04eba1376832384898caefcdcec67dac2101536f
kevinJ <jaipersaudk@gmail.com>
2019-08-29 13:35:19 -0400

    Finished Chapter 2. Onto Chapter 3

--- Chap.1-4/Chapter_2.java
+++ Chap.1-4/Chapter_2.java
@@ -4,7 +4,9 @@
 //                      - the "+" is essentially <<
 // Booleans - datatype is boolean --> similar to C++ as bool
 
+import java.util.Scanner;
 
+/*
 public class Countdown {
   public static void main (String[] args) {
     // the following inputs are all printed on the same line
@@ -22,11 +24,50 @@ public class Countdown {
     // Difference between concantenation and addition
     System.out.println("24 and 45 concantenated: " + 24 + 45);
     System.out.println("24 and 45 added: " + (24+45));
+  }
+}
+*/
 
+/*
+public class Inputs {
+  public static void main (String[] args) {
     // Variables
     int keys = 88;
     final int MAX = 45; // this values cannot be changed once set (uppercase used for constant values) --> similar to const
     System.out.print("A piano has " + keys + " keys!\n");
-    System.out.print(MAX);
+    System.out.println(MAX);
+
+    // Inputs
+    /* to write inputs, you must include the library at the beginning of the program - "import java.util.Scanner;"
+     * you must create a Scanner object by using the new operator
+     * nextLine reads all the input and returns it all as one string
+     * use nextInt or nextDouble to insert a integer or double value
+     */
+
+/*
+    String message;
+    Scanner scan = new Scanner (System.in);
+    System.out.println("Enter a message: ");
+    message = scan.nextLine();
+    System.out.println("You entered: \"" + message + "\" ");
+
+    int mileage;
+    double gallons, mpg;
+    Scanner input = new Scanner (System.in);
+    System.out.print("Enter the number of miles: ");
+    mileage = input.nextInt();
+    System.out.print("Enter the number of gallons: ");
+    gallons = input.nextDouble();
+    mpg = mileage / gallons;
+    System.out.println("Your MPG is: " + mpg);
   }
 }
+*/
+
+/*
+public class Exercises {
+  public static void main (String[] args) {
+
+  }
+}
+*/
