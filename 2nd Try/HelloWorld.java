adf1a0cc9ce2b51e6fe20c3336c9f0e1b4dc73de
kevinJ <jaipersaudk@gmail.com>
2020-02-03 16:37:24 -0500

    Notes

--- 2nd Try/HelloWorld.java
+++ 2nd Try/HelloWorld.java
@@ -1,5 +1,47 @@
+/*
 public class HelloWorld {
   public static void main (String[] args){
-    System.out.println("HelloWorld. My name is Kevin");
+    System.out.println("HelloWorld"); // java is strictly object oriented
+  } //end of main method
+} //end of class HelloWorld
+*/
+
+// Lecture 2/3
+/* Command Prompt: java Test arg0 arg1
+ * Program: String[arg0 arg1...] args
+ */
+
+/*
+ * only one public class per source file
+ * can have multiple classes in a source file, but it cannot be public
+ */
+
+/*
+public class HelloWorld {
+  public static void main (String[] args) {
+    System.out.println("Hello World");
   }
 }
+
+class Test1 {
+  public static void main (String[] args) {
+    System.out.println("This is a test");
+  }
+}
+*/
+
+/* Escape Characters - Modifies strings
+ * \n
+ * \t
+ * \r
+ * \\
+ * \"
+ */
+
+/*
+public class HelloWorld {
+  public static void main (String[] args) {
+
+  }
+}
+*/
